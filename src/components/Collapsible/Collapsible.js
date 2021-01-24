21195ae65cd6f2cea443b3b8235a1f63dcd456f6
letisen <letisendev@gmail.com>
2021-01-24 10:59:08 -0500

    Plan section reset

--- src/components/Collapsible/Collapsible.js
+++ src/components/Collapsible/Collapsible.js
@@ -8,14 +8,7 @@ class Collapsible extends React.Component {
   render() {
     return (
       <div className="container">
-        <ul id="tabs-swipe-demo" className="tabs">
-          <li className="tab col s3"><a href="#test-swipe-1">Test 1</a></li>
-          <li className="tab col s3"><a class="active" href="#test-swipe-2">Test 2</a></li>
-          <li className="tab col s3"><a href="#test-swipe-3">Test 3</a></li>
-        </ul>
-        <div id="test-swipe-1" className="col s12 blue">Test 1</div>
-        <div id="test-swipe-2" className="col s12 red">Test 2</div>
-        <div id="test-swipe-3" className="col s12 green">Test 3</div>
+        <h1>Planes</h1>
       </div>
     );
   }
