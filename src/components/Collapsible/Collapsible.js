3ce49dddd29bc51e372412eb248d53235187432f
letisen <letisendev@gmail.com>
2021-01-24 10:56:14 -0500

    first commit of development branch

--- src/components/Collapsible/Collapsible.js
+++ src/components/Collapsible/Collapsible.js
@@ -1,24 +1,92 @@
 import React from 'react'
 
+const Item1 = () => {
+  return <h1>1</h1>
+}
+const Item2 = () => {
+  return <h1>2</h1>
+}
+const Item3 = () => {
 
-class Collapsible extends React.Component {
+  return <h1>3</h1>
+}
+const Item4 = () => {
+  return <h1>4</h1>
+}
 
-  
 
-  render() {
-    return (
-      <div className="container">
-        <ul id="tabs-swipe-demo" className="tabs">
-          <li className="tab col s3"><a href="#test-swipe-1">Test 1</a></li>
-          <li className="tab col s3"><a class="active" href="#test-swipe-2">Test 2</a></li>
-          <li className="tab col s3"><a href="#test-swipe-3">Test 3</a></li>
-        </ul>
-        <div id="test-swipe-1" className="col s12 blue">Test 1</div>
-        <div id="test-swipe-2" className="col s12 red">Test 2</div>
-        <div id="test-swipe-3" className="col s12 green">Test 3</div>
-      </div>
-    );
-  }
+const Check = () => {
+  document.addEventListener('DOMContentLoaded', function () {
+
+    const inputs = document.getElementById('inputs');
+    console.log(inputs);
+    for (const x of inputs) {
+      if (x.checked == true) {
+        switch (x.id) {
+          case 'basico':
+            return <Item1/>;
+
+          case 'avanzado':
+            return <Item2/>;
+
+          case 'premium':
+            return <Item3/>;
+
+          case 'full':
+            return <Item4/>;
+
+        }
+      }
+    }
+
+  });
+}
+
+
+const Collapsible = () => {
+
+
+
+
+
+
+  return (
+    <div className="container">
+      <form id="inputs">
+        <p>
+          <label htmlFor="basico">
+            <input id="basico" name="group1" type="radio" />
+            <span>Básico</span>
+          </label>
+        </p>
+        <p>
+          <label htmlFor="avanzado">
+            <input id="avanzado" name="group1" type="radio" />
+            <span>Avanzado</span>
+          </label>
+        </p>
+        <p>
+          <label htmlFor="premium">
+            <input id="premium" name="group1" type="radio" />
+            <span>Premium</span>
+          </label>
+        </p>
+        <p>
+          <label htmlFor="full">
+            <input id="full" name="group1" type="radio" />
+            <span>Full</span>
+          </label>
+        </p>
+      </form>
+
+
+      <Check />
+
+
+
+    </div>
+  );
+
 }
 
 export default Collapsible;
